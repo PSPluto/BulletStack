43373d8c315b0a8a45edc250d6b3718b6cffdbbc
Pluto <aokitaisei217@gmail.com>
2026-04-14 11:07:10 +0900

    敵の出現できるようになった

---
+++ Assets/scripts/Enemy/EnemySpawner.cs
@@ -0,0 +1,82 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] GameObject Enemy1;
+
+    public void SpawnEnemy()
+    {
+        GameObject obj = Instantiate(Enemy1, new Vector3(Random.Range(-2, 3), 10, 0), Quaternion.identity);
+    }
+    Coroutine _activeLoop;
+    void Start()
+    {
+
+    }
+    void OnEnable()
+    {
+        StateManager.OnStateChanged += StateChanged;
+    }
+    void OnDisable()
+    {
+        StateManager.OnStateChanged -= StateChanged;
+    }
+
+    // state更新のイベントで呼び出される
+    public void StateChanged(int newState)
+    {
+        // 今のループを止める
+        if (_activeLoop != null)
+        {
+            StopCoroutine(_activeLoop);
+            _activeLoop = null;
+        }
+        // 次のループを始める
+        switch (newState)
+        {
+            case 0: _activeLoop = StartCoroutine(TitleLoop()); break;
+            case 1: _activeLoop = StartCoroutine(InGameLoop()); break;
+            case 2: _activeLoop = StartCoroutine(GameOverLoop()); break;
+            default: break;
+        }
+    }
+    IEnumerator TitleLoop()
+    {
+        while (true)
+        {
+            // タイトルのループ処理
+            yield return null;
+        }
+    }
+
+    IEnumerator InGameLoop()
+    {
+        int count = 0;
+        while (true)
+        {
+            if(count >= 200)
+            {
+                count = 0;
+                SpawnEnemy();
+            }
+            else
+            {
+                count += 1;
+            }
+            yield return null;
+        }
+    }
+
+    IEnumerator GameOverLoop()
+    {
+        while (true)
+        {
+            // ゲームオーバーのループ処理
+            yield return null;
+        }
+    }
+}
+
+
