d23a2d868abab47ffe36848bcc375f642db5f03b
Pluto <aokitaisei217@gmail.com>
2026-05-07 12:07:20 +0900

    エラー修正

---
+++ Assets/scripts/Utility/IngameManager.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IngameManager : MonoBehaviour
+{
+    public int wave;
+    Coroutine _activeLoop;
+    void Start()
+    {
+
+    }
+    void OnEnable()
+    {
+        StateManager.OnStateChanged += StateChanged;
+    }
+    void OnDisable()
+    {
+        StateManager.OnStateChanged -= StateChanged;
+    }
+
+    // state更新のイベントで呼び出される
+    public void StateChanged(int newState)
+    {
+        // 今のループを止める
+        if (_activeLoop != null)
+        {
+            StopCoroutine(_activeLoop);
+            _activeLoop = null;
+        }
+        // 次のループを始める
+        switch (newState)
+        {
+            case 0: _activeLoop = StartCoroutine(TitleLoop()); break;
+            case 1: _activeLoop = StartCoroutine(InGameLoop()); break;
+            case 2: _activeLoop = StartCoroutine(GameOverLoop()); break;
+            default: break;
+        }
+    }
+    IEnumerator TitleLoop()
+    {
+        while (true)
+        {
+            // タイトルのループ処理
+            yield return null;
+        }
+    }
+
+    IEnumerator InGameLoop()
+    {
+        Initialize();
+        while (true)
+        {
+            
+            yield return null;
+        }
+    }
+
+    IEnumerator GameOverLoop()
+    {
+        while (true)
+        {
+            // ゲームオーバーのループ処理
+            yield return null;
+        }
+    }
+
+    public void Initialize()
+    {
+        wave = 0;
+
+    }
+}
