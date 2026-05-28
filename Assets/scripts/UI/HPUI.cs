0ecc0b8bd727f3ea1b68a25d8b6775fddbdf5afc
Pluto <aokitaisei217@gmail.com>
2026-05-28 23:11:22 +0900

    HPバーの画面更新チェック追加

--- Assets/scripts/UI/HPUI.cs
+++ Assets/scripts/UI/HPUI.cs
@@ -5,16 +5,13 @@ public class HPUI : MonoBehaviour
 {
     public MotherBoard player;
     [SerializeField] Image hpBarImage;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public float lastHP = -1f;
 
-    // Update is called once per frame
     void Update()
     {
+        if (player.currentHP == lastHP) return;
         float mappingHP = player.currentHP / player.maxHP;
         hpBarImage.fillAmount = Mathf.Lerp(0.14f, 0.93f, mappingHP);
+        lastHP = player.currentHP;
     }
 }
